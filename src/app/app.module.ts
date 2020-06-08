75bdd46ff561f3dd7aeb4474ac885c12745dfcaf
GrayUnit <grayunit@outlook.fr>
2020-06-08 16:25:54 +0200

    Ajout des composants icons & création de la base d'ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
