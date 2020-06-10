f8b3fc73a8537c7348b9544fec63f3844725b24d
GrayUnit <grayunit@outlook.fr>
2020-06-10 14:54:38 +0200

    Création d'un btn générique et utilisation de @Output

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { OrdersService } from '../../services/orders.service';
 import { Order } from 'src/app/shared/models/orders';
 import { StateOrder } from 'src/app/shared/enums/state-order.enum';
+import { Btn } from 'src/app/shared/interfaces/btn-i';
 
 @Component({
   selector: 'app-page-list-orders',
@@ -13,11 +14,27 @@ export class PageListOrdersComponent implements OnInit {
   public collection: Order[];
   public headers: string[];
   public states = Object.values(StateOrder);
+  public btnRoute: Btn;
+  public btnHref: Btn;
+  public btnAction: Btn;
+
 
   constructor(private os: OrdersService) {
   }
 
   ngOnInit(): void {
+    this.btnRoute = {
+      label: "Add an order",
+      route: "add"
+    };
+    this.btnHref = {
+      label: "Go to google",
+      href: "http://www.google.fr"
+    };
+    this.btnAction = {
+      label: "Open dialogue",
+      action: true
+    };
     this.os.collection.subscribe((datas) => {
       this.collection = datas;
     });
@@ -36,6 +53,10 @@ export class PageListOrdersComponent implements OnInit {
     this.os.changeState(item, event.target.value).subscribe((result) => {
       item.state = result.state;
     });
+  }
+
+  public openPopUp() {
+    console.log("Open popup");
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { TableLightComponent } from './components/table-light/table-light.component';
 import { BtnComponent } from './components/btn/btn.component';
 import { TotalPipe } from './pipes/total.pipe';
@@ -11,7 +12,8 @@ import { TableDarkComponent } from './components/table-dark/table-dark.component
 @NgModule({
   declarations: [TableLightComponent, BtnComponent, TotalPipe, StateDirective, TableDarkComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [BtnComponent, TableLightComponent, TableDarkComponent, TotalPipe, StateDirective]
 })
